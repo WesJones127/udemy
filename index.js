46e7223ba227fb81ce5ac0f35fa9bae049fe9196
Wes Jones <wjones127@gmail.com>
2022-11-27 08:50:38 -0500

    ui

--- index.js
+++ index.js
@@ -19,6 +19,9 @@ app.use(
 app.use(passport.initialize());
 app.use(passport.session());
 
+app.get("/", (req, res) => {
+  res.send("home");
+});
 require("./routes/authRoutes")(app);
 
 const PORT = process.env.PORT || 5000;
