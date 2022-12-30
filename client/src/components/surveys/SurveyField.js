1d2bfed161c8ee142d3371de10e876b174898975
Wes Jones <wjones127@gmail.com>
2022-12-30 18:33:35 -0500

    creating survey form

---
+++ client/src/components/surveys/SurveyField.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+export default ({ input, label }) => {
+  console.log(input);
+
+  return (
+    <div>
+      <label>{label}</label>
+      <input {...input} />
+    </div>
+  );
+};
