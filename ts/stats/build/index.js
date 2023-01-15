12c0b4ff99bc1dfd05af4afbee7f042e875180b9
Wes Jones <wjones127@gmail.com>
2023-01-15 17:08:33 -0500

    merge fix

--- ts/stats/build/index.js
+++ ts/stats/build/index.js
@@ -1,8 +1,12 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
+<<<<<<< HEAD
 const MatchReader_1 = require("./MatchReader");
 const Summary_1 = require("./Summary");
 const matchReader = MatchReader_1.MatchReader.fromCsv('football.csv');
 const summary = Summary_1.Summary.winsAnalysisWithHtmlReport('Man United');
 matchReader.load();
 summary.buildAndPrintReport(matchReader.matches);
+=======
+console.log("hi");
+>>>>>>> 4db7686 (stats)
