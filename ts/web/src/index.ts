c26992a3ffc2d36197b4386d60ef30396232ae7c
Wes Jones <wjones127@gmail.com>
2023-02-05 22:24:35 -0500

    web framework working, only missing collectionView

--- ts/web/src/index.ts
+++ ts/web/src/index.ts
@@ -1,15 +1,15 @@
 import { User, UserProps } from "./models/User";
 import axios, { AxiosResponse } from "axios";
 import { Collection } from "./models/Collection";
-import { UserForm } from "./views/UserForm";
+import { UserEdit } from "./views/UserEdit";
 
 const user = User.buildUser({ name: "Name", age: 20 });
 const root = document.getElementById("root");
 
 if (root) {
-  const userForm = new UserForm(root, user);
+  const userEdit = new UserEdit(root, user);
 
-  userForm.render();
+  userEdit.render();
 } else {
   throw new Error("root element not found");
 }
--- ts/web/src/views/View.ts
+++ ts/web/src/views/View.ts
@@ -2,11 +2,17 @@ import { Model } from "../models/Model";
 import { HasId } from "../models/Model";
 
 export abstract class View<T extends Model<K>, K extends HasId> {
+  regions: { [key: string]: Element } = {};
+
   constructor(public parent: Element, public model: T) {
     this.bindModel();
   }
 
   abstract template(): string;
+
+  regionsMap(): { [key: string]: string } {
+    return {};
+  }
 
   eventsMap(): { [key: string]: () => void } {
     return {};
@@ -28,6 +34,21 @@ export abstract class View<T extends Model<K>, K extends HasId> {
       });
     }
   }
+
+  mapRegions(fragment: DocumentFragment): void {
+    const regionsMap = this.regionsMap();
+
+    for (let key in regionsMap) {
+      const selector = regionsMap[key];
+      const element = fragment.querySelector(selector);
+      if (element) {
+        this.regions[key] = element;
+      }
+    }
+  }
+
+  onRender(): void {}
+
   render(): void {
     this.parent.innerHTML = "";
 
@@ -35,6 +56,9 @@ export abstract class View<T extends Model<K>, K extends HasId> {
     templateElement.innerHTML = this.template();
 
     this.bindEvents(templateElement.content);
+    this.mapRegions(templateElement.content);
+
+    this.onRender();
 
     this.parent.append(templateElement.content);
   }
